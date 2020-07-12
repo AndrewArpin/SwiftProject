7f59553de29f6d30f404f67584612226020c501b
Anadrew <200398857@student.georgianc.on.ca>
2020-07-12 17:56:52 -0400

    Initial Commit

---
+++ Note Application/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Note Application
+//
+//  Created by Andrew Arpin on 2020-07-12.
+//  Copyright © 2020 Andrew Arpin. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ Note ApplicationTests/Note_ApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Note_ApplicationTests.swift
+//  Note ApplicationTests
+//
+//  Created by Andrew Arpin on 2020-07-12.
+//  Copyright © 2020 Andrew Arpin. All rights reserved.
+//
+
+import XCTest
+@testable import Note_Application
+
+class Note_ApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
